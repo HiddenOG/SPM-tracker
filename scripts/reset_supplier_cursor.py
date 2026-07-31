8507da82b4f0587f2f2797c4e88c9f6149379d6c
Mr OG <godsonodelegend@gmail.com>
2026-07-31 11:21:07 +0100

    Delete scripts directory

--- scripts/reset_supplier_cursor.py
+++
@@ -1,5 +0,0 @@
-﻿import sys
-sys.path.insert(0, 'scripts')
-from db import get_client
-get_client().table('sync_state').delete().eq('account', 'gmail_supplier').execute()
-print('Reset gmail_supplier cursor')
